c29da84b9227e285abced932449cb3b8c3f34e4e
lizardmech <lizardmech@gmail.com>
2026-07-21 12:48:17 +1000

    Remove orphaned mode 56 shader graph

--- neo/shaders/builtin/pathtracing/pathtrace_restir_indirect_initial_producer.rt.hlsl
+++
@@ -1,3 +0,0 @@
-#define RB_PT_ENABLE_RESTIR 1
-#define RB_PT_RESTIR_INDIRECT_INITIAL_PRODUCER_ONLY 1
-#include "pathtrace_smoke.rt.hlsl"
--- neo/shaders/builtin/pathtracing/pathtrace_restir_reflection_producer.rt.hlsl
+++
@@ -1,6 +0,0 @@
-#define RB_PT_ENABLE_RESTIR 1
-#define RB_PT_ENABLE_RESTIR_TEMPORAL 1
-#define RB_PT_ENABLE_RESTIR_SPATIAL 1
-#define RB_PT_RESTIR_REFLECTION_PRODUCER_ONLY 1
-#define RB_PT_FORCE_DEBUG_MODE 56u
-#include "pathtrace_smoke.rt.hlsl"
